320ca35b1b0f94370c25a2e681d7b3f2d7d3fc24
AceKun <acekun141@gmail.com>
2019-10-09 02:21:38 +0700

    commit

--- blog/home/models.py
+++ blog/home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+import uuid
 # Create your models here.
+class Post(models.Model):
+    uuid = str(uuid.uuid4())
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    image = models.ImageField(upload_to='images/', null=True)
+    time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
